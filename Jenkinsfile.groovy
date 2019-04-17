4795008f94e63708db7b3ce92486e76aee2fab27
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 17:39:54 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,18 @@
+node("master"){
+  stage ('Build') {
+ 
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/webapp.git'
+    withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+      maven: 'M3'){
+        
+    bat 'mvn -Dmaven.test.failure.ignore=true clean package'
+    }
+  }
+  
+  
+  stage ('deploy')
+  {
+    bat '''copy C:\\Apps\\Jenkins\\jobs\\WebApp-Pipe\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
+  }
+}
