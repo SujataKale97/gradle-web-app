90daa2b4fbfd2eba5c7991f15a9eef134b3ccecc
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 17:43:59 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,12 +2,10 @@ node("master"){
   stage ('Build') {
  
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git'
-    withGradle(
-        // Maven installation declared in the Jenkins "Global Tool Configuration"
-      gradle: 'LocalGradle'){
+    
         
     bat 'gradlew.bat clean build'
-    }
+   
   }
   
   
