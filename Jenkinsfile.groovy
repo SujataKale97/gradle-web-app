a49f86d24eb94938ff78deae3258c79706cdcb14
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 17:41:31 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,17 +2,17 @@ node("master"){
   stage ('Build') {
  
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/webapp.git'
-    withMaven(
+    withGradle(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
-      maven: 'M3'){
+      gradle: 'LocalGradle'){
         
-    bat 'mvn -Dmaven.test.failure.ignore=true clean package'
+    bat 'gradlew.bat clean build'
     }
   }
   
   
   stage ('deploy')
   {
-    bat '''copy C:\\Apps\\Jenkins\\jobs\\WebApp-Pipe\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
+    //bat '''copy C:\\Apps\\Jenkins\\jobs\\WebApp-Pipe\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
   }
 }
