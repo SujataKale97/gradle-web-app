7a282c3b1150559ed803a0908084d72410b9a814
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-19 11:05:38 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,6 +9,12 @@ node("master"){
     bat 'gradle war'
    
   }
+  stage ('deploy')
+  {
+    bat '''copy C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace\\build\\libs\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
+  }
+  
+  
   
   
   
