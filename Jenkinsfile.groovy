4b2b2c8813a7124cb5bea960cd118e19f6356b3f
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-19 15:23:50 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,20 +5,22 @@ pipeline{
   }
   stages{
   stage ('Build') {
- 
+    steps{
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git'
     
         
     bat 'gradle clean build'
     
     bat 'gradle war'
-   
+    }
   }
   stage ('deploy')
   {
+    steps{
     bat '''copy C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace\\build\\libs\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
     
     bat 'echo myCustomEnvVar=$myCustomEnvVar'
+    }
   }
   
   
