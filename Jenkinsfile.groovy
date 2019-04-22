bb71235cfd69fcb641c79007cd1c56fde4ceda32
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-22 11:01:04 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,6 @@
 pipeline{
   
-  agent{
-    dockerfile true
-  }
-  stages{
+  
   stage ('Build') {
     steps{
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git'
@@ -24,7 +21,7 @@ pipeline{
   }
   
   
-  }
+  
   
   
 }
