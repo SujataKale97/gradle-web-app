584d144dd469f7326963613ccdc3cd4dab08a304
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-30 17:05:33 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,11 +6,11 @@ node{
         
     bat 'gradle clean build'
     
-   bat '''copy C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace\\build\\libs\\*.war C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace'''
+  //  bat '''copy C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace\\build\\libs\\*.war C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace'''
       
   }
   stage('Deploy'){
-    bat 'docker build -t mywebapp  .'
+ //   bat 'docker build -t mywebapp  .'
   }
   
   stage('Test'){
