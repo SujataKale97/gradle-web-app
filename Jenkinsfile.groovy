e4a7a0ae546c6f517fb07b3d12a656deb7ea21e3
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-09-18 10:30:30 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node{
    
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/gradle-web-app.git' 
         
-    bat 'gradle clean build'
+    bat 'gradle clean build docker'
     
 // bat '''copy C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace\\build\\libs\\*.war C:\\Apps\\Jenkins\\jobs\\gradle-pipeline\\workspace'''
       
